7de34d4efc4b36678a6335b7caa56e5955052d13
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 00:41:25 +0100

    Pipe out to null

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -e
 
-# export CLUSTER_ID="$(terraform output cluster_id)" && \
-# echo "Setting cluster-id:${CLUSTER_ID}" && \
-# curl -X GET \
-#     -H "Content-Type: application/json" \
-#     -H "Authorization: Bearer ${DO_TOKEN}" \
-#     "https://api.digitalocean.com/v2/kubernetes/clusters/${CLUSTER_ID}/kubeconfig" \
-# > cluster_config
-
 sudo snap install doctl
 doctl auth init -t $DO_TOKEN
-doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config
+doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config > /dev/null
