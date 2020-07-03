67d32fe4929094a277e62804ab24b9015c70a344
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 00:33:37 +0100

    Trying to use the cli

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-export CLUSTER_ID="$(terraform output cluster_id)" && \
-echo "Setting cluster-id:${CLUSTER_ID}" && \
-curl -X GET \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${DO_TOKEN}" \
-    "https://api.digitalocean.com/v2/kubernetes/clusters/${CLUSTER_ID}/kubeconfig" \
-> cluster_config
+# export CLUSTER_ID="$(terraform output cluster_id)" && \
+# echo "Setting cluster-id:${CLUSTER_ID}" && \
+# curl -X GET \
+#     -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer ${DO_TOKEN}" \
+#     "https://api.digitalocean.com/v2/kubernetes/clusters/${CLUSTER_ID}/kubeconfig" \
+# > cluster_config
+
+snap install doctl
+doctl auth init -t $DO_TOKEN
+doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config
