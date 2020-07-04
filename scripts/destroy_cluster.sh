aa6a8f73e40d9d8b3b9bd48c4dde496e54661dde
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 22:47:35 +0100

    post destruction steps

--- scripts/destroy_cluster.sh
+++ scripts/destroy_cluster.sh
@@ -11,6 +11,7 @@ then
     helm uninstall nginx
     terraform init -backend-config="access_key=${DO_ACCESS_KEY}" -backend-config="secret_key=${DO_SECRET_KEY}"
     terraform destroy -var "do_token=${DO_TOKEN}" -auto-approve
+    python3 ./scripts/post_cluster_destruction.py
 else
     echo "Cluster not marked for deletion"
 fi
