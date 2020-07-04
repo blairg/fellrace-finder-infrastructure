49f3efc7a7798b16b3167b8a29f54816bed37534
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 16:46:26 +0100

    Using digital ocean cli

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -3,6 +3,4 @@ set -e
 
 sudo snap install doctl
 doctl auth init -t $DO_TOKEN
-doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config > /dev/null
-
-export KUBECONFIG="$(pwd)/cluster_config"
+doctl kubernetes cluster kubeconfig save bgtest-cluster
