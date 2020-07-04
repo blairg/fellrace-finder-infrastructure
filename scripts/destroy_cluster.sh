5b53ece1fa0b7f2057ab37f33857ae6c03b8f0ab
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 20:54:24 +0100

    Initialize the backend before destroying

--- scripts/destroy_cluster.sh
+++ scripts/destroy_cluster.sh
@@ -1,4 +1,5 @@
 set -e
 
 helm uninstall nginx
+terraform init -backend-config="access_key=${DO_ACCESS_KEY}" -backend-config="secret_key=${DO_SECRET_KEY}"
 terraform destroy -var "do_token=${DO_TOKEN}" -auto-approve
