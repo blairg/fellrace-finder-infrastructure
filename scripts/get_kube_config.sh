0953324cfe35b1903bea6296c6587344248f3069
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 16:59:30 +0100

    run as std user

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -2,6 +2,6 @@
 set -e
 
 sudo snap install doctl
-sudo doctl auth init -t $DO_TOKEN
-sudo snap connect doctl:kube-config
-sudo doctl kubernetes cluster kubeconfig save bgtest-cluster
+doctl auth init -t $DO_TOKEN
+snap connect doctl:kube-config
+doctl kubernetes cluster kubeconfig save bgtest-cluster
