4cf1d8892f99dc3ae7193f4595061c4e7edb69dc
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 16:42:24 +0100

    erroneouis symbol

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -5,4 +5,4 @@ sudo snap install doctl
 doctl auth init -t $DO_TOKEN
 doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config > /dev/null
 
-export $KUBECONFIG="$(pwd)/cluster_config"
+export KUBECONFIG="$(pwd)/cluster_config"
