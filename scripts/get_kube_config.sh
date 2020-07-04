ca37a44827dd89b747a0eac2a20b30f1f5e7623a
Blair Garrett <blair.garrett1@gmail.com>
2020-07-04 16:37:06 +0100

    some debug info

--- scripts/get_kube_config.sh
+++ scripts/get_kube_config.sh
@@ -4,3 +4,6 @@ set -e
 sudo snap install doctl
 doctl auth init -t $DO_TOKEN
 doctl kubernetes cluster kubeconfig show bgtest-cluster | tee cluster_config > /dev/null
+
+echo "in getting kube config"
+pwd
--- scripts/install_nginx_ingress.sh
+++ scripts/install_nginx_ingress.sh
@@ -2,6 +2,8 @@ set -e
 
 KUBECONFIG="$(pwd)/cluster_config"
 
+echo $KUBECONFIG
+
 helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 helm install nginx nginx-stable/nginx-ingress | 1>&2 echo "NGINX Ingress already installed"
